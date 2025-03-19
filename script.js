38eecee402f5c8fc0618ee536a0c806c2cf4583e
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-03-19 23:08:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name
+		this.age=age
+	}
+	get name(){
+		return this.name
+	}
+	age(age){
+		this.age=age
+		return this.age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name){
+		super(this.name)
+	}
+	study(){
+		console.log(`${this.name} is studing`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+		constructor(name){
+		super(this.name)
+	}
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
